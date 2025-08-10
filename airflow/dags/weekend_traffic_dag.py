2cafa4ecc583b29e5e290908e4393614fb5c4f86
KARYL\Owner <qj860301@gmail.com>
2025-08-11 01:34:53 +0800

    hotix-dag schedule

--- airflow/dags/weekend_traffic_dag.py
+++ airflow/dags/weekend_traffic_dag.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 import pendulum
 from airflow.models.dag import DAG
+from airflow.operators.python import PythonOperator
 
 # 從 tasks/crawler.py 匯入你需要的任務函式
 from tasks.crawler import scrape_and_filter_data, load_to_bigquery
 
 with DAG(
     dag_id="weekend_traffic_analysis",
-    schedule="*/5 * * * *",
+    # 將排程設定為每分鐘執行一次
+    schedule="* * * * *", 
     start_date=pendulum.datetime(2025, 8, 8, tz="Asia/Taipei"),
     catchup=False,
     tags=["traffic", "crawler", "weekend"],
